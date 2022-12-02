875cfa2e24880a8c131e8be26692718d6d158624
Mariusz Kryński <mrk@sed.pl>
2022-12-02 10:55:26 +0000

    day02: lookup tables

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -3,66 +3,31 @@
 utils::entry!(main);
 use ufmt_stdio::*;
 
-type Score = i32;
+type Score = i16;
+type Pair = u8;
 
-const ROCK: u8 = 1;
-const PAPER: u8 = 2;
-const SCISSORS: u8 = 3;
+const SCORES: [u8; 11] = [3, 6, 0, 0, 0, 3, 6, 0, 6, 0, 3];
+const THINGS: [u8; 11] = [2, 0, 1, 0, 0, 1, 2, 0, 1, 2, 0];
 
-const LOSE: u8 = 1;
-const DRAW: u8 = 2;
-const WIN: u8 = 3;
-
-fn parse(code: u8) -> u8 {
-    return if code <= b'C' {
-        code - b'A' + 1
-    } else {
-        code - b'X' + 1
-    }
+fn parse_line(line: &[u8]) -> Pair {
+    return (line[0] - b'A') * 4 + line[2] - b'X'
 }
 
-fn parse_line(line: &[u8]) -> (u8, u8) {
-    return (parse(line[0]), parse(line[2]))
+fn score(pair: Pair) -> Score {
+    (SCORES[pair as usize] + 1 + (pair & 3)) as Score
 }
 
-
-fn score(pair: (u8, u8)) -> Score {
-    let outcome = match pair {
-        (ROCK, PAPER) => 6,
-        (ROCK, SCISSORS) => 0,
-        (PAPER, ROCK) => 0,
-        (PAPER, SCISSORS) => 6,
-        (SCISSORS, ROCK) => 6,
-        (SCISSORS, PAPER) => 0,
-        _ => 3,
-    };
-    pair.1 as Score + outcome
-}
-
-fn determine_thing(thing: u8, result: u8) -> u8 {
-    match (thing, result) {
-        (ROCK, LOSE) => SCISSORS,
-        (ROCK, DRAW) => ROCK,
-        (ROCK, WIN) => PAPER,
-        (PAPER, LOSE) => ROCK,
-        (PAPER, DRAW) => PAPER,
-        (PAPER, WIN) => SCISSORS,
-        (SCISSORS, LOSE) => PAPER,
-        (SCISSORS, DRAW) => SCISSORS,
-        (SCISSORS, WIN) => ROCK,
-        _ => panic!(),
-    }
-}
-
-fn score2(pair: (u8, u8)) -> Score {
-    let thing = determine_thing(pair.0, pair.1);
-    score((pair.0, thing))
+fn score2(pair: Pair) -> Score {
+    score((pair & !3) + THINGS[pair as usize])
 }
 
 fn main() {
     let input = utils::iter_lines!("input.txt").map(parse_line);
     let part1 = input.clone().map(score).sum::<Score>();
     let part2 = input.map(score2).sum::<Score>();
+
+    assert!(part1 == 13268);
+    assert!(part2 == 15508);
 
     println!("part1: {}", part1);
     println!("part2: {}", part2);
