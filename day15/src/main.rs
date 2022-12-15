ddce2ed628bd311f7340befe170478cec93cc156
Mariusz Kryński <mrk@sed.pl>
2022-12-15 17:54:59 +0000

    day15: second part

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -7,6 +7,7 @@ extern crate alloc;
 
 use alloc::vec::Vec;
 use itertools::Itertools;
+#[allow(unused_imports)]
 use mos_alloc;
 use ufmt_stdio::{ufmt::derive::uDebug, *};
 use utils::to_str;
@@ -21,7 +22,21 @@ fn manhatan_dist(p1: (i32, i32), p2: (i32, i32)) -> i32 {
     return (p1.0 - p2.0).abs() + (p1.1 - p2.1).abs();
 }
 
+fn perimeter(mid: (i32, i32), r: i32) -> impl Iterator<Item = (i32, i32)> {
+    (0..r).flat_map(move |v| {
+        [
+            (mid.0 + v, mid.1 - r + v),
+            (mid.0 + r - v, mid.1 + v),
+            (mid.0 - v, mid.0 + r - v),
+            (mid.0 - r + v, mid.1 - v),
+        ]
+    })
+}
+
 fn main() {
+    const PART2_SIZE: i32 = 4000000;
+    const PART1_ROW: i32 = 2000000;
+
     let sensors = utils::iter_lines!("../../input/day15/input.txt")
         .map(|line| {
             line.split(|c| !c.is_ascii_digit() && *c != b'-')
@@ -41,26 +56,39 @@ fn main() {
     let mut min_y = i32::MAX;
     let mut max_x = i32::MIN;
     let mut max_y = i32::MIN;
+    let mut max_dist = i32::MIN;
 
     for sensor in &sensors {
         min_x = min_x.min(sensor.sensor.0.min(sensor.becon.0));
         min_y = min_y.min(sensor.sensor.1.min(sensor.becon.1));
         max_x = max_x.max(sensor.sensor.0.max(sensor.becon.0));
         max_y = max_y.max(sensor.sensor.1.max(sensor.becon.1));
+        max_dist = max_dist.max(sensor.dist);
     }
 
-    println!("{:?} {:?}", (min_x, min_y), (max_x, max_y));
+    let all_outside = |pos: &(i32, i32)| {
+        sensors
+            .iter()
+            .map(|s| manhatan_dist(*pos, s.sensor) > s.dist && s.becon != *pos)
+            .all(|f| f)
+    };
 
-    println!("input: {:?}", sensors.as_slice());
-    let part1 = (-5000000..5000000)
-        .map(|x| (x, 2000000))
-        .map(|pos| {
-            sensors
-                .iter()
-                .map(|s| manhatan_dist(pos, s.sensor) <= s.dist && s.becon != pos)
-                .any(|f| f) as usize
-        })
-        .sum::<usize>();
-
+    let part1 = (-min_x - max_dist..max_x + max_dist)
+        .map(|x| (x, PART1_ROW))
+        .map(|pos| !all_outside(&pos) as i32)
+        .sum::<i32>();
+    assert!(part1 == 5100464);
     println!("PART1: {}", part1);
+
+    let out = sensors
+        .iter()
+        .flat_map(|s| perimeter(s.sensor, s.dist + 1))
+        .filter(|v| v.0 >= 0 && v.0 <= PART2_SIZE && v.1 >= 0 && v.1 <= PART2_SIZE)
+        .filter(all_outside)
+        .next()
+        .unwrap();
+
+    let part2 = out.0 as i64 * 4000000 + out.1 as i64;
+    assert!(part2 == 11557863040754);
+    println!("PART2: {:?}", part2);
 }
