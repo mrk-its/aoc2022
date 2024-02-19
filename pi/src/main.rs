1f34061aedefcdce53eb233c7cfbbce066d27d55
Mariusz Kryński <mrk@samba.tv>
2024-02-19 11:56:44 +0100

    linting

--- pi/src/main.rs
+++ pi/src/main.rs
@@ -4,19 +4,21 @@ utils::entry!(main);
 
 use ufmt_stdio::*;
 
-#[cfg(feature="to_int_unchecked")]
+#[cfg(feature = "to_int_unchecked")]
 fn to_int(value: f32) -> i32 {
-    unsafe {
-        value.to_int_unchecked::<i32>()
-    }
+    unsafe { value.to_int_unchecked::<i32>() }
 }
 
-#[cfg(not(feature="to_int_unchecked"))]
+#[cfg(not(feature = "to_int_unchecked"))]
 fn to_int(value: f32) -> i32 {
     value as i32
 }
 
 fn main() {
-    let pi = 4.0 * (0..666).map(|i| i * 2 + 1).map(|i| (1.0 - ((i & 2) as f32)) / (i as f32)).sum::<f32>();
+    let pi = 4.0
+        * (0..666)
+            .map(|i| i * 2 + 1)
+            .map(|i| (1.0 - ((i & 2) as f32)) / (i as f32))
+            .sum::<f32>();
     ufmt_stdio::println!("100 pi: {}", to_int(100.0 * pi));
 }
