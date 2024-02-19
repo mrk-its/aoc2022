554d84302984139c7963f52a88150155e53e3dea
Mariusz Kryński <mrk@samba.tv>
2024-02-19 13:47:33 +0100

    use timeout, temp disable long-running

--- run_all.sh
+++ run_all.sh
@@ -1,11 +1,12 @@
 cd $(dirname $0)
 set -e
-PROGS="day01 day02 day03 day04 day05 day06 day07 day08 day09 day10 day11 day12 day13 day14 day17 day18"
-for cmd in build run; do
+# PROGS="day01 day02 day03 day04 day05 day06 day07 day08 day09 day10 day11 day12 day13 day14 day17 day18"
+PROGS="day01 day02 day03 day04 day06 day07 day08 day09 day10 day13 day17 day18"
+for cmd in "cargo build" "timeout -v 5m cargo run"; do
   for prg in $PROGS; do
-    for opts in "" "--release"; do
-        echo cargo $cmd -p $prg $opts
-        cargo $cmd -p $prg $opts
+    for opts in "--release"; do
+        echo $cmd -p $prg $opts
+        $cmd -p $prg $opts
     done
   done;
 done
