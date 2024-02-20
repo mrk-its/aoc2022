3c3149dc5b11199d3ecf14d95e409128a1ef5a9d
Mariusz Kryński <mrk@samba.tv>
2024-02-20 14:33:07 +0100

    inc timeout

--- run_all.sh
+++ run_all.sh
@@ -2,7 +2,7 @@ cd $(dirname $0)
 set -e
 PROGS="day01 day02 day03 day04 day05 day06 day07 day08 day09 day10 day11 day12 day13 day14 day17 day18"
 # PROGS="day01 day02 day03 day04 day06 day07 day08 day09 day10 day13 day17 day18"
-for cmd in "cargo build" "timeout -v 5m cargo run"; do
+for cmd in "cargo build" "timeout -v 10m cargo run"; do
   for prg in $PROGS; do
     for opts in "" "--release"; do
         echo $cmd -p $prg $opts
