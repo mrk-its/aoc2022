cb959a65328b9fc4b4ebe4e0bd4114bb1b302d77
Mariusz Kryński <mrk@samba.tv>
2024-02-20 09:06:22 +0100

    test_all.sh: build debug and release

--- run_all.sh
+++ run_all.sh
@@ -4,7 +4,7 @@ PROGS="day01 day02 day03 day04 day05 day06 day07 day08 day09 day10 day11 day12 d
 # PROGS="day01 day02 day03 day04 day06 day07 day08 day09 day10 day13 day17 day18"
 for cmd in "cargo build" "timeout -v 5m cargo run"; do
   for prg in $PROGS; do
-    for opts in "--release"; do
+    for opts in "" "--release"; do
         echo $cmd -p $prg $opts
         $cmd -p $prg $opts
     done
