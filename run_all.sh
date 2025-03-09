07041e6adba3d8f3cdeed49d90f09f0b4f6bae9e
Mariusz Kryński <mrk@samba.tv>
2025-03-09 20:28:28 +0100

    enable day13, disable day07

--- run_all.sh
+++ run_all.sh
@@ -1,7 +1,7 @@
 cd $(dirname $0)
 set -e
 # PROGS="day01 day02 day03 day04 day05 day06 day07 day08 day09 day10 day12 day13 day14 day17 day18"
-PROGS="day01 day02 day03 day04 day05 day06 day07 day08 day09 day10 day12 day14 day17 day18"
+PROGS="day01 day02 day03 day04 day05 day06 day08 day09 day10 day12 day13 day14 day17 day18"
 
 for cmd in "cargo build" "timeout -v 10m cargo run"; do
   for prg in $PROGS; do
